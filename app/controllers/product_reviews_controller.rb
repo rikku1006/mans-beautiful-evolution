7eb2ea60e8b4cac7257bf2a3623d55a2a8630773
rikku1006 <r1006s0125@icloud.com>
2021-05-23 21:12:12 +0900

    レビュー商品投稿に関するコントローラー記述

--- app/controllers/product_reviews_controller.rb
+++ app/controllers/product_reviews_controller.rb
@@ -1,2 +1,24 @@
 class ProductReviewsController < ApplicationController
+  def index
+  end
+
+  def new
+    @product_review = ProductReview.new
+  end
+
+  def create
+    @product_review = ProductReview.new(product_review_params)
+    if @product_review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_review_params
+    params.require(:product_review).permit(:image, :product_name, :category_id, :review_comment).merge(user_id: current_user.id)
+  end
 end
