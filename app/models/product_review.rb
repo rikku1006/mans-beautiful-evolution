a5b610b5fae5817ba87fbe18b5e8dcef98e3e337
rikku1006 <r1006s0125@icloud.com>
2021-05-25 19:11:43 +0900

    コメントモデル作成、他のモデルにアソシエーション記述

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,6 +1,7 @@
 class ProductReview < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :comments
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
