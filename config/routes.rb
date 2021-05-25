eca9c4eae28ee85dc014c5fc5597b731e298edfb
rikku1006 <r1006s0125@icloud.com>
2021-05-25 19:13:16 +0900

    product_reviewsに、commentsをネスト化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "product_reviews#index"
-  resources :product_reviews
+  resources :product_reviews do
+    resources :comments, only: [:create]
+  end
 end
